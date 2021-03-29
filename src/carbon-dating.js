f80446e463255bfd3db7fd69b7d4e3c8eb6d4950
Finnchick <sergik3001@gmail.com>
2021-03-29 20:42:06 +0300

    fix: change solutions for new tests

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,23 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const LN2 = 0.693;
 
 module.exports = function dateSample(sampleActivity) {
-  if (+sampleActivity % 10 || +sampleActivity === 0 || +sampleActivity < 0 || +sampleActivity > 100) {
-    return false;
-  }
-  if(parseInt(sampleActivity) != sampleActivity) {
-    return false;
-  }
-if (typeof(sampleActivity) != "string") {
-  return false;
-}
+  if (
+    typeof sampleActivity !== 'string'
+    || (/[A-Za-z]/g).test(sampleActivity)
+    || sampleActivity <= 0
+    || sampleActivity > MODERN_ACTIVITY) { return false; }
 
-let k = 0.693/HALF_LIFE_PERIOD;
-
-let t = Math.log10(MODERN_ACTIVITY/sampleActivity)/k;
-
-return Math.ceil(t);
-
+  return Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / (LN2 / HALF_LIFE_PERIOD));
 };
+
+
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,22 +1,17 @@
 module.exports = function repeater(str, options) {
-  let res = '';
-  if (options) {
-      const times = options.repeatTimes || 1;
-      let addition = typeof options.addition === 'boolean' || options.addition === null ? String(options.addition) : options.addition || '';
-      const separator = options.separator || '+';
-      const additionRepeat = options.additionRepeatTimes || 1;
-      const additionSeparator = options.additionSeparator || '|';
-      let additionPattern = addition;
-      if (additionRepeat > 1) {
-          additionPattern = additionSeparator + addition;
-      }
-      const pattern = str + addition + additionPattern.repeat(additionRepeat - 1);
-      for (let i = 1; i <= times; i++) {
-          res += pattern;
-          if (i < times) {
-              res += separator;
-          }
-      }
-  }
-  return res;
+  let {
+    repeatTimes = 1,
+    separator = "+",
+    addition = "",
+    additionRepeatTimes = 1,
+    additionSeparator = "|",
+  } = options;
+
+  return (
+    String(str) +
+    (addition + additionSeparator).repeat(additionRepeatTimes).slice(0, -Number(additionSeparator.length)) +
+    separator
+  )
+    .repeat(repeatTimes)
+    .slice(0, -Number(separator.length));
 };
